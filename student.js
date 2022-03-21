0cae06f882025bae4791314900a5c74f7bd2323b
Sean <mckeags7@gmail.com>
2022-03-21 17:49:14 -0400

    object mapping

--- student.js
+++ student.js
@@ -1,78 +1,78 @@
-// const CSV_DATA = [
-//     {
+// var CSV_DATA = new Array()
+//     [
 //       name: 'Sally Student',
 //       id: 'SOO1',
 //       assignment: 'Assignment 1',
 //       score: 95
-//     },
+    
 
-//     {
+    
 //         name: 'Johnny Readerman',
 //         id: 'S002',
 //         assignment: 'Assignment 1',
 //         score: 71
-//     },
+    
 
-//     {
+    
 //         name:'Tony Thinkerson',
 //         id: 'S003',
 //         assignment: 'Assignment 1',
 //         score: 88
-//     },
+    
 
-//     {
+    
 //         name: 'Johnny Readerman',
 //         id: 'S002',
 //         assignment: 'Assignment 2',
 //         score: 90
-//     },
+   
 
-//     {
+    
 //         name: 'Sally Student',
 //         id: 'S001',
 //         assignment: 'Assignment 2',
 //         score: 100
-//     },
+   
 
-//     {
+    
 //         name: 'Tony Thinkerson',
 //         id: 'S003',
 //         assignment: 'Assignment 2',
 //         score: 57
-//     },
+    
 
-//     {
+   
 //         name: 'Sally Student',
 //         id: 'S001',
 //         assignment: 'Assignment 3',
 //         score: 85
-//     },
+   
 
-//     {
+    
 //         name: 'Tony Thinkerson',
 //         id: 'S003',
 //         assignment: 'Assignment 3',
-//         score: 80
-//     },
+//         score: 80,
+    
 
-//     {
+    
 //         name: 'Johnny Readerman',
 //         id: 'S002',
 //         assignment: 'Assignment 3',
 //         score: 0
-//     }
+    
 //   ];
 
-var CSV_DATA = new Array('name', 'id','score') 
-student[0] = newAssignment("Sally Student", "S001", 95); 
-student[1] = newAssignment("Johnny Readerman", "S002", 71); 
-student[2] = newAssignment("Tony Thinkerson", "S003", 88); 
-student[3] = newAssignment("Johnny Readerman", "S002", 90); 
-student[4] = newAssignment("Sally Student", "S001", 100); 
-student[5] = newAssignment("Tony Thinkerson", "S003", 57); 
-student[6] = newAssignment("Sally Student", "S001", 85); 
-student[7] = newAssignment("Tony Thinkeson", "S003", 80);
-student[8] = newAssignment("Johnny Readerman", "S002", 0);  
+// var CSV_DATA = new Array('name', 'id','score') 
+// student[0] = newAssignment("Sally Student", "S001", 95); 
+// student[1] = newAssignment("Johnny Readerman", "S002", 71); 
+// student[2] = newAssignment("Tony Thinkerson", "S003", 88); 
+// student[3] = newAssignment("Johnny Readerman", "S002", 90); 
+// student[4] = newAssignment("Sally Student", "S001", 100); 
+// student[5] = newAssignment("Tony Thinkerson", "S003", 57); 
+// student[6] = newAssignment("Sally Student", "S001", 85); 
+// student[7] = newAssignment("Tony Thinkeson", "S003", 80);
+// student[8] = newAssignment("Johnny Readerman", "S002", 0);  
 
 // var CSV_DATA = 
 
@@ -105,56 +105,42 @@ student[8] = newAssignment("Johnny Readerman", "S002", 0);
 //   grade[8][2] = 0
 
 
-  function newAssignment( name, id, score ) { 
+//   function newAssignment( name, id, score ) { 
   
-    this.name = name; 
-    this.id = id; 
-    this.score = score; 
-} 
+//     this.name = name; 
+//     this.id = id; 
+//     this.score = score; 
+// } 
 
-CSV_DATA.forEach(function(score) {
-    var row = document.createElement("tr");
-    score.array.forEach(score, {
-        getElementById
-    });(function(score) {
-      var cell = document.createElement("td");
-      cell.textContent = score;
-      row.appendChild(cell);
-    });
-    table.appendChild(row);
-  });
 
-  for (var i=0; i<5; i++) {
-    document.write("<tr><td>Number " + i + " is:</td>");
-    document.write("<td>" + CSV_DATA[i] + "</td></tr>");
-  }
+var CSV_DATA = new Array ({001: {name: 'Sally Student', score: 15}, 002: {name: 'Johnny Readerman', score: 5}, 003: {name: 'Tony Thinkerson', score: 16}})
 
-function avgRec(score, i, n)
-{
-     
-    // Last element
-    if (i == n - 1)
-        return score[i];
- 
-    // When index is 0, divide sum computed so
-    // far by n.
-    if (i == 0)
-        return ((score[i] + avgRec(score, i + 1, n)) / n);
- 
-    // Compute sum
-    return (score[i] + avgRec(score, i + 1, n));
-}
- 
-// Function that return average of an array.
-function average(score, n)
-{
-    return avgRec(score, 0, n);
-}
- 
-// Driver code
-let arr = [ 95, 100, 85 ];
-let n = arr.length;
- 
-document.write(average(arr, n));
+const result = Object.entries(obj1).reduce(
+  // group is the key from obj1, e.g. "A"
+  // properties is the object for that key, e.g. { bottom: 63, mid: 4, top: 15 }
+  (acc, [id, {properties}]) => [
+    ...acc, { id, ...properties }
+  ], []
+);
+
+console.log(result);
+
+
+Object.values(CSV_DATA.reduce((acc, curr) => {
+    if(acc[curr.id]) {
+      acc[curr.id].scores+=curr.score
+    }
+    else {
+      acc[curr.id] = {
+        name: curr.name,
+        score: curr.score
+      }
+    }
+  
+    return acc
+  }, {})).map(item => ({
+    name: item.name,
+    grade: transformScoreToGrade(item.score) 
+  }))
  
 
